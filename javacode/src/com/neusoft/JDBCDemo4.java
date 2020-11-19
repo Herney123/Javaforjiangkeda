55d8e4136135b29e900b86938a25dedbe148ce43
gw <61634433+Herney123@users.noreply.github.com>
2020-11-19 10:38:14 +0800

    1119-01

--- javacode/src/com/neusoft/JDBCDemo4.java
+++ javacode/src/com/neusoft/JDBCDemo4.java
@@ -2,12 +2,13 @@ package com.neusoft;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
- * 插入操作
+ * 查询操作
  */
-public class JDBCDemo2 {
+public class JDBCDemo4 {
     public static void main(String[] args) throws Exception {
         //        1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         //        2、 注册驱动
@@ -16,17 +17,26 @@ public class JDBCDemo2 {
         Connection conn = DriverManager.getConnection("jdbc:mysql://" +
                 "localhost:3306/jiangkeda","root", "12345678");
         //        4、定义sql
-        String sql = "insert into account values (null , '田七', 223)";
+        String sql = "select * from account";
         //        5、获取数据库连接对象statement
         Statement stmt = conn.createStatement();
         //        6、执行sql
-        int i = stmt.executeUpdate(sql);
+        ResultSet rs = stmt.executeQuery(sql);
         //        7、处理结果
-        if(i==1){
-            System.out.println("插入成功");
-        }else {
-            System.out.println("插入失败");
+//        while (rs.next()){
+//            int id = rs.getInt(1);
+//            String name = rs.getString(2);
+//            double balance = rs.getDouble(3);
+//            System.out.println(id+"----"+name+"----"+balance);
+//        }
+        while (rs.next()){
+            int id = rs.getInt("ID");
+            String name = rs.getString("NAME");
+            double balance = rs.getDouble("balance");
+            System.out.println(id+"----"+name+"----"+balance);
         }
+
+
         //        8、释放资源
         stmt.close();
         conn.close();
